2e04af589904d87f4070f766b5bb17ff1b008560
Harri Lainio <harri.lainio@op.fi>
2021-11-01 19:11:17 +0200

    support for seqNo for memledger

--- addons/memledger.go
+++ addons/memledger.go
@@ -1,6 +1,8 @@
 package addons
 
 import (
+	"strconv"
+	"strings"
 	"sync"
 
 	"github.com/findy-network/findy-wrapper-go/plugin"
@@ -14,8 +16,14 @@ const memName = "FINDY_MEM_LEDGER"
 // and some development cases.
 type Mem struct {
 	Mem struct {
-		sync.RWMutex
+		sync.Mutex
 		Ory map[string]string
+	}
+
+	// Seq is seqNo in real Indy ledger, by this we get correct behaviour
+	Seq struct {
+		sync.Mutex
+		No uint
 	}
 }
 
@@ -25,26 +33,67 @@ func (m *Mem) Close() {
 
 func (m *Mem) Open(name ...string) bool {
 	resetMem()
+	m.incSeqNo()
 	return name[0] == memName
 }
 
-func (m *Mem) Write(_ plugin.TxInfo, ID, data string) error {
+func (m *Mem) Write(tx plugin.TxInfo, ID, data string) error {
+	//	if !tx.Update && tx.TxType == plugin.TxTypeSchema {
+	//		glog.V(1).Infoln("----------- debugging -------")
+	//		return nil
+	//	}
+
 	m.Mem.Lock()
 	defer m.Mem.Unlock()
+
+	m.incSeqNo()
 	m.Mem.Ory[ID] = data
 	return nil
 }
 
-func (m *Mem) Read(_ plugin.TxInfo, ID string) (name string, value string, err error) {
-	m.Mem.RLock()
-	defer m.Mem.RUnlock()
+func (m *Mem) Read(tx plugin.TxInfo, ID string) (name string, value string, err error) {
+	m.Mem.Lock()
+	defer m.Mem.Unlock()
+
+	seqNo := m.SeqNo()
+	curval := m.Mem.Ory[ID]
+
+	// if reading first time ("null" exists in "seqNo:"), replace it with the
+	// current seqNo. This mimics indy ledger behaviour
+	if tx.TxType == plugin.TxTypeSchema && strings.Contains(curval, "null") {
+		repval := strings.Replace(curval, "null", strconv.Itoa(int(seqNo)), 1)
+		m.Mem.Ory[ID] = repval
+	}
+
 	return ID, m.Mem.Ory[ID], nil
 }
 
-var memLedger = &Mem{Mem: struct {
-	sync.RWMutex
-	Ory map[string]string
-}{}}
+func (m *Mem) incSeqNo() {
+	m.Seq.Lock()
+	defer m.Seq.Unlock()
+
+	m.Seq.No++
+}
+
+func (m *Mem) SeqNo() uint {
+	m.Seq.Lock()
+	defer m.Seq.Unlock()
+
+	return m.Seq.No
+}
+
+var memLedger = &Mem{
+	Mem: struct {
+		sync.Mutex
+		Ory map[string]string
+	}{},
+	Seq: struct {
+		sync.Mutex
+		No uint
+	}{
+		No: 4, // Just installed empty Indy ledger starts about from here
+	},
+}
 
 func init() {
 	pool.RegisterPlugin(memName, memLedger)
